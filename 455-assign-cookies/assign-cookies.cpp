e47b0618cf6e36b5b4562a4616bef95ed7c5be1d
BEDANT-LOHIA <bedantlohia@gmail.com>
2026-07-05 17:14:16 +0530

    Time: 8 ms (58.91%) | Memory: 44.7 MB (78.18%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int l = 0;
+        int r = 0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while(l < s.size() && r<g.size()){
+            if(g[r] <= s[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+};
